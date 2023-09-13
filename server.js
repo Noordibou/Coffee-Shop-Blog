0687bd5868ebd74905fcd4bd835525dd6cddf89b
noordibou <noor.dibou@hotmail.com>
2023-09-12 23:52:16 -0500

    fixing db

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const mongoose = require('mongoose');
 const CoffeeShop = require('./models/coffeeShop')
 const favicon = require('serve-favicon');
 const logger = require('morgan');
