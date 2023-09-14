9fa7930ad46dd5220f2f79cbe084b570a1257798
noordibou <noor.dibou@hotmail.com>
2023-09-14 00:15:02 -0500

    test3

--- client/src/pages/CoffeeShopsPage/CoffeeShopsPage.jsx
+++ client/src/pages/CoffeeShopsPage/CoffeeShopsPage.jsx
@@ -7,7 +7,7 @@ export default function CoffeeShopsPage() {
 
   const getCoffeeShop = () => {
     axios
-      .get('http://localhost:3000/coffeeshops')
+      .get('https://coffee-shop-blog-server.vercel.app/coffeeshops')
       .then((response) => setCoffeeShop(response.data))
       .catch((error) => console.log(error));
   };
