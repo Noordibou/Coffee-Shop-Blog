e71ddf853f6d50fc10a9f88f90cf1cc8eb2dc27d
noordibou <noor.dibou@hotmail.com>
2023-09-13 23:18:18 -0500

    test3

--- api/index.js
+++ api/index.js
@@ -1,23 +1,25 @@
 const express = require('express');
-const path = require('path');
+// const path = require('path');
 const mongoose = require('mongoose');
 const CoffeeShop = require('./models/coffeeShop')
-const favicon = require('serve-favicon');
-const logger = require('morgan');
+// const favicon = require('serve-favicon');
+// const logger = require('morgan');
+const cors = require('cors')
 
 require('dotenv').config();
 // Connect to the database
-require('./config/database');
+// require('./config/database');
    
 const app = express();
    
-app.use(logger('dev'));
+// app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
 
 // Configure both serve-favicon & static middleware
 // to serve from the production 'build' folder
 // app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
 
 // Put API routes here, before the "catch all" route
@@ -54,18 +56,21 @@ app.delete('/coffeeshops/:id', (req, res)=>{
 
 // The following "catch all" route (note the *) is necessary
 // to return the index.html on all non-AJAX requests
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
-app.get('/*', function(req, res) {
-  res.send('Server is running')
-})
+// app.get('/*', function(req, res) {
+//   res.send('Server is running')
+// })
 
 // Configure to use port 3001 instead of 3000 during
 // development to avoid collision with React's dev server
-const port = process.env.PORT || 3001;
+app.listen(3000, ()=>{
+  console.log('listening...');
+});
 
-app.listen(port, function() {
- console.log(`Express app running on port ${port}`)
+mongoose.connect(process.env.DATABASE_URL)
+mongoose.connection.once('open', ()=>{
+  console.log('connected to mongod...');
 });
