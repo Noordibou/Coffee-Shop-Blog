21d8e65d06c9d8e39ad2dd7458c047a2aa2a5657
noordibou <noor.dibou@hotmail.com>
2023-09-28 19:02:56 -0500

    test

--- client/src/context/UserContext.jsx
+++ client/src/context/UserContext.jsx
@@ -1,28 +1,31 @@
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
-export const UserContext = createContext({}); 
 
-export function UserContextProvider({ children }) {
-    const [user, setUser] = useState(null);
+export const UserContext=createContext({})
 
-    
-    const getUser = async () => {
-        try {
-            const res = await axios.get('/auth/refetch', { withCredentials: true });
-            setUser(res.data);
-        } catch (err) {
-            console.error("Error fetching user data:", err);
-            console.error("Response data:", err.response?.data);
-        }
+
+export function UserContextProvider({children}){
+    const [user,setUser]=useState(null)
+
+    useEffect(()=>{
+      getUser()
+
+    },[])
+
+    const getUser=async()=>{
+      try{
+        const res=await axios.get("https://coffee-shop-blog-server.vercel.app/auth/refetch",{withCredentials:true})
+        // console.log(res.data)
+        setUser(res.data)
+
+      }
+      catch(err){
+        console.log(err)
+      }
     }
-    useEffect(() => {
-        getUser();
-    }, []);
     
-    return (
-        <UserContext.Provider value={{ user, setUser }}>
-            {children}
-        </UserContext.Provider>
-    );
+    return (<UserContext.Provider value={{user,setUser}}>
+      {children}
+    </UserContext.Provider>)
 }
