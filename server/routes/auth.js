28b1b1b65fcf95e8d717b2fa1b1f55ec498bc9cf
noordibou <noor.dibou@hotmail.com>
2023-09-29 23:13:49 -0500

    going back to old code

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -7,17 +7,17 @@ const { createSecretToken } = require('../util/secretToken')
 
 
 //REGISTER
-router.post("/register",async(req,res)=>{
-    try{
-        const {username,email,password}=req.body
-        const salt=await bcrypt.genSalt(10)
-        const hashedPassword= bcrypt.hashSync(password,salt)
-        const newUser=new User({username,email,password:hashedPassword})
-        const savedUser=await newUser.save()
+router.post("/register", async (req, res) => {
+    try {
+        const { username, email, password } = req.body
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = bcrypt.hashSync(password, salt)
+        const newUser = new User({ username, email, password: hashedPassword })
+        const savedUser = await newUser.save()
         res.status(200).json(savedUser)
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 
@@ -36,50 +36,51 @@ router.post("/login", async (req, res) => {
         if (!match) {
             return res.status(401).json("Wrong credentials!")
         }
-        const token = createSecretToken({id: user._id, username: user.username, email: user.email});
+        const token = jwt.sign({ _id: user._id, username: user.username, email: user.email }, process.env.TOKEN_KEY, {
+            expiresIn: 3 * 24 * 60 * 60,
+        });
         const { password, ...info } = user._doc
-//         res.cookie("token", token, {
-//             withCredentials: true,
-//             httpOnly: false,
-//         }).status(200).json(info);
+        //         res.cookie("token", token, {
+        //             withCredentials: true,
+        //             httpOnly: false,
+        //         }).status(200).json(info);
 
-//     }
-//     catch (err) {
-//         res.status(500).json(err)
-//     }
-// })
-res.cookie("token", token, {
-    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
-    httpOnly: false,
-    sameSite: "none",
-    secure: true,
+        //     }
+        //     catch (err) {
+        //         res.status(500).json(err)
+        //     }
+        // })
+        res.cookie("token", token, {
+            httpOnly: false,
+            sameSite: "none",
+            secure: true,
 
-  });
+        });
 
-  res.status(200).json(info);
-} catch (error) {
-  console.error("Error during login:", error);
-  res.status(500).json({ error: "An error occurred" });
-}
+        res.status(200).json(info);
+    } catch (error) {
+        console.error("Error during login:", error);
+        res.status(500).json({ error: "An error occurred" });
+    }
 });
 
 
 //LOGOUT
-router.get("/logout",async (req,res)=>{
-    try{
-        res.clearCookie("token",{sameSite:"none",secure:true}).status(200).send("User logged out successfully!")
+router.get("/logout", async (req, res) => {
+    try {
+        res.clearCookie("token", { sameSite: "none", secure: true }).status(200).send("User logged out successfully!")
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 })
 
 //REFETCH USER
-router.get("/refetch", (req,res)=>{
-    const token=req.cookies.token
-    jwt.verify(token,process.env.TOKEN_KEY,{},async (err,data)=>{
-        if(err){
+router.get("/refetch", (req, res) => {
+    const token = req.cookies.token
+    jwt.verify(token, process.env.TOKEN_KEY, {}, async (err, data) => {
+        if (err) {
             return res.status(404).json(err)
         }
         res.status(200).json(data)
