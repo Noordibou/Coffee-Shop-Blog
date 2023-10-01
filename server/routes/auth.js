eb5c99e564f933dfa6a1daf0664678c8f8249ff9
noordibou <noor.dibou@hotmail.com>
2023-09-30 21:20:55 -0500

    test2

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,86 +1,69 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/User')
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const { createSecretToken } = require('../util/secretToken')
+const express=require('express')
+const router=express.Router()
+const User=require('../models/User')
+const bcrypt=require('bcrypt')
+const jwt=require('jsonwebtoken')
 
 
 //REGISTER
-router.post("/register", async (req, res) => {
-    try {
-        const { username, email, password } = req.body
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = bcrypt.hashSync(password, salt)
-        const newUser = new User({ username, email, password: hashedPassword })
-        const savedUser = await newUser.save()
+router.post("/register",async(req,res)=>{
+    try{
+        const {username,email,password}=req.body
+        const salt=await bcrypt.genSalt(10)
+        const hashedPassword=bcrypt.hashSync(password,salt)
+        const newUser=new User({username,email,password:hashedPassword})
+        const savedUser=await newUser.save()
         res.status(200).json(savedUser)
 
     }
-    catch (err) {
+    catch(err){
         res.status(500).json(err)
     }
 
 })
 
-//LOGIN
-router.post("/login", async (req, res) => {
-    try {
-        const user = await User.findOne({ email: req.body.email })
 
-        if (!user) {
+//LOGIN
+router.post("/login",async (req,res)=>{
+    try{
+        const user=await User.findOne({email:req.body.email})
+       
+        if(!user){
             return res.status(404).json("User not found!")
         }
-        const match = await bcrypt.compare(req.body.password, user.password)
-
-        if (!match) {
+        const match=await bcrypt.compare(req.body.password,user.password)
+        
+        if(!match){
             return res.status(401).json("Wrong credentials!")
         }
-        const token = jwt.sign({ _id: user._id, username: user.username, email: user.email }, process.env.TOKEN_KEY, {
-            expiresIn: 3 * 24 * 60 * 60,
-        });
-        const { password, ...info } = user._doc
-        //         res.cookie("token", token, {
-        //             withCredentials: true,
-        //             httpOnly: false,
-        //         }).status(200).json(info);
+        const token=jwt.sign({_id:user._id,username:user.username,email:user.email},process.env.SECRET,{expiresIn:"3d"})
+        const {password,...info}=user._doc
+        res.cookie("token",token).status(200).json(info)
 
-        //     }
-        //     catch (err) {
-        //         res.status(500).json(err)
-        //     }
-        // })
-        res.cookie("token", token, {
-            httpOnly: false,
-            sameSite: "none",
-            secure: true,
-
-        });
-
-        res.status(200).json(info);
-    } catch (error) {
-        console.error("Error during login:", error);
-        res.status(500).json({ error: "An error occurred" });
     }
-});
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
 
 
 //LOGOUT
-router.get("/logout", async (req, res) => {
-    try {
-        res.clearCookie("token", { sameSite: "none", secure: true }).status(200).send("User logged out successfully!")
+router.get("/logout",async (req,res)=>{
+    try{
+        res.clearCookie("token",{sameSite:"none",secure:true}).status(200).send("User logged out successfully!")
 
     }
-    catch (err) {
+    catch(err){
         res.status(500).json(err)
     }
 })
 
 //REFETCH USER
-router.get("/refetch", (req, res) => {
-    const token = req.cookies.token
-    jwt.verify(token, process.env.TOKEN_KEY, {}, async (err, data) => {
-        if (err) {
+router.get("/refetch", (req,res)=>{
+    const token=req.cookies.token
+    jwt.verify(token,process.env.SECRET,{},async (err,data)=>{
+        if(err){
             return res.status(404).json(err)
         }
         res.status(200).json(data)
@@ -88,4 +71,5 @@ router.get("/refetch", (req, res) => {
 })
 
 
-module.exports = router
+
+module.exports=router
