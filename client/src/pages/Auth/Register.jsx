ca7fc35039ea2e1c919e23245d57682e953fb410
noordibou <noor.dibou@hotmail.com>
2023-09-30 21:31:52 -0500

    test3

--- client/src/pages/Auth/Register.jsx
+++ client/src/pages/Auth/Register.jsx
@@ -1,98 +1,59 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom'; 
-import URL from '../../URL';
+import { Link, useNavigate } from "react-router-dom"
+import { useState } from "react"
+import axios from 'axios'
+import URL from '../../URL'
 
-export default function Register() {
-  const navigate = useNavigate(); 
 
-  const [formData, setFormData] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
+const Register = () => {
+  const [username,setUsername]=useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+  const [error,setError]=useState(false)
+  const navigate=useNavigate()
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post(URL+`/auth/register`, formData);
-      console.log('Registration successful:', response.data);
-      navigate('/login'); 
-    } catch (error) {
-      console.error('Registration failed:', error.response.data);
+  const handleRegister=async ()=>{
+    
+    try{
+      const res=await axios.post(URL+"/auth/register",{username,email,password})
+      setUsername(res.data.username)
+      setEmail(res.data.email)
+      setPassword(res.data.password)
+      setError(false)
+      navigate("/login")
+      
     }
-  };
+    catch(err){
+      setError(true)
+      console.log(err)
+    }
+
+  }
+
+  
+
   return (
-    <main className="bg-gray-100 flex justify-center pt-44 h-screen">
-      <div className="w-96">
-        <h2 className="text-2xl font-semibold mb-4 text-center">Sign Up</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="username" className="block text-gray-600">
-              Username
-            </label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              value={formData.username}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Username"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="email" className="block text-gray-600">
-              Email
-            </label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Email"
-              required
-            />
-          </div>
-          <div className="mb-6">
-            <label htmlFor="password" className="block text-gray-600">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              className="w-full px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              placeholder="Password"
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <button
-              type="submit"
-              className="w-full bg-bodyColor text-white py-2 rounded-md hover:bg-gray-600 focus:outline-none focus:ring focus:border-blue-300"
-            >
-              Sign Up
-            </button>
-          </div>
-        </form>
-        <p className="text-center text-gray-600">
-          Already have an account? <a href="/login" className="text-blue-500">Log in</a>
-        </p>
-      </div>
-    </main>
-  );
+    <>
+      <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
+    <h1 className="text-lg md:text-xl font-extrabold"><Link to="/">Blog Market</Link></h1>
+    <h3><Link to="/login">Login</Link></h3>
+    </div>
+    <div className="w-full flex justify-center items-center h-[80vh] ">
+       <div className="flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]">
+         <h1 className="text-xl font-bold text-left">Create an account</h1>
+         <input onChange={(e)=>setUsername(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your username" />
+         <input onChange={(e)=>setEmail(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="text" placeholder="Enter your email" />
+         <input onChange={(e)=>setPassword(e.target.value)} className="w-full px-4 py-2 border-2 border-black outline-0" type="password" placeholder="Enter your password" />
+         <button onClick={handleRegister} className="w-full px-4 py-4 text-lg font-bold text-white bg-black rounded-lg hover:bg-gray-500 hover:text-black ">Register</button>
+         {error && <h3 className="text-red-500 text-sm ">Something went wrong</h3>}
+         <div className="flex justify-center items-center space-x-3">
+          <p>Already have an account?</p>
+          <p className="text-gray-500 hover:text-black"><Link to="/login">Login</Link></p>
+         </div>
+       </div>
+    </div>
+    </>
+    
+  )
 }
+
+export default Register
